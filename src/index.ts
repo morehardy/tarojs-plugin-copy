7bda4600fa741c09b756461650e52968fb12d616
hardyhe <hardyhe@xwfintech.com>
2021-10-19 17:14:05 +0800

    feat: index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,37 @@
 import * as fs from 'fs'
+import remove from './remove'
+import copy from './copy'
+import type { IPluginContext } from '@tarojs/service'
 
-console.log(fs)
+class PluginOptions {
+  public fromPath: string = ''
+  public outputPath: string = ''
+  public once: boolean = false
+}
+
+function getTriggerFun(once) {
+  return once ? 'onBuildComplete' : 'onBuildFinish'
+}
+
+const voidErrorMap = new Map([
+  ['fromPath', 'fromPath can not be empty'],
+  ['outputPath', 'outputPath can not be empty'],
+])
+
+export default function (ctx: IPluginContext, options: PluginOptions) {
+  const defaultOptions = new PluginOptions()
+  const fromPath = options.fromPath || defaultOptions.fromPath
+  const outputPath = options.outputPath || defaultOptions.outputPath
+  const once = options.once || defaultOptions.once 
+  const triggerFunction = getTriggerFun(once)
+
+  for (let errorItem of voidErrorMap) {
+    const [key, msg] = errorItem
+    if (!options[key]) throw new Error(msg)
+  }
+
+  ctx[triggerFunction](() => {
+    if (fs.existsSync(outputPath)) remove(outputPath)
+    copy(fromPath, outputPath)
+  })
+}
